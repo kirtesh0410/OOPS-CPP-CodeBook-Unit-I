f62ca1186885ce0264814a234acab9bd99153440
Kirtesh Bharambe <kirteshbharambe0410@gmail.com>
2026-09-23 03:17:17 +0530

    Create Program03.cpp

---
+++ Program03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {85, 76, 92, 68, 89};
+
+    for (int i = 0; i < 5; i++) {
+        cout << marks[i] << " ";
+    }
+
+    return 0;
+}
