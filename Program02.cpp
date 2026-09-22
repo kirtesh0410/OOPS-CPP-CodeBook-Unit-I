1988d1459a9d75a03604b00a56eb07a9022dd0c0
Kirtesh Bharambe <kirteshbharambe0410@gmail.com>
2026-09-23 03:16:36 +0530

    Create Program02.cpp

---
+++ Program02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks = 72;
+
+    if (marks >= 40) {
+        cout << "Pass";
+    } else {
+        cout << "Fail";
+    }
+
+    return 0;
+}
