275032253107695229efedd084094c883404f437
Kirtesh Bharambe <kirteshbharambe0410@gmail.com>
2026-09-23 03:15:41 +0530

    Create Program01.cpp

---
+++ Program01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int roll = 205;
+    char grade = 'B';
+    float fee = 18500.75;
+
+    cout << "Roll No: " << roll << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Fee: " << fee << endl;
+
+    return 0;
+}
